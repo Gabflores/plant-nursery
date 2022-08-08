010ece9cc656954566a1a40d2602bb3538e5e3c6
dev-renatto-pantoja <floresdelsante@gmail.com>
2022-08-07 23:10:32 -0500

    Add Banner and Header components

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,18 +6,25 @@ function Banner(){
 //<img  classname="bannerImg  h-4/6" src={bannerImg} alt="banner"/>
   return (
     <>
-    <div className="flex pt-10 h-auto md:h-screen overflow-hidden z-10" style={{backgroundImage: `url(${bannerImg})`, backgroundSize:"cover", backgroundRepeat:"no-repeat", backgroundPosition:"right bottom"}}>
+    <div 
+    className="flex pt-10 h-auto md:h-screen overflow-hidden z-10" 
+    style={
+      {backgroundImage: `url(${bannerImg})`, 
+      backgroundSize:"cover", 
+      backgroundRepeat:"no-repeat", 
+      backgroundPosition:"right bottom"}
+      }>
       
       <div className="py-12 pl-4 w-full  md:pl-9 md:py-20 xl:pl-40 lg:py-36" >
         <h2 className="bannerText text-2xl md:text-6xl lg:text-8xl">Best house</h2>
-        <h2 className="bannerText text-2xl md:text-6xl lg:text-8xl  md:mb-12">plants varieties.</h2>
+        <h2 className="bannerText text-2xl md:text-6xl lg:text-8xl  md:mb-12">plants varieties</h2>
 
-        <p className="hidden text-white text-center px-5 py-2 bg-black rounded-3xl md:inline md:text-xl md:px-11 md:py-3.5 ">Shop me</p>
+        <p className="hidden text-white text-center px-5 py-2 bg-black rounded-3xl md:inline md:text-xl md:px-11 md:py-3.5 cursor-pointer text-center">Shop me</p>
         <h3 className="mt-7 text-sm md:mt-16 md:font-medium md:text-2xl lg:text-2xl">Beautiful living greenery</h3>
         <h3 className="text-sm md:font-medium md:text-2xl lg:text-2xl">for homes and offices</h3>
 
         <p className="mt-5 text-xs font-extralight mb-4 md:mb-2.5 md:text-base xlg:text-sm">We've been mentioned in press</p>
-        <p className="inline text-white text-center  px-5 py-2 bg-black rounded-3xl md:hidden">Shop me</p>
+        <p className="inline text-white text-center  px-5 py-2 bg-black rounded-3xl md:hidden cursor-pointer" >Shop me</p>
         <div className="mt-6 flex"> 
           <p className="text-sm text-gray-500 md:text-2xl lg:text-4xl">Kodigo</p>
           <p className="ml-3 text-sm text-gray-500 md:text-2xl md:ml-20 lg:text-4xl">Applaudo Studios</p>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,20 +11,20 @@ function Header() {
   return (
     <>
     <header className="flex justify-between w-screen items-end px-4 md:px-12 py-3 md:py-6 fixed bg-white z-100 shadow-md shadow-gray-200">
-      <img className=" w-32 " src={logo} alt="logo "/>
+      <img className=" w-32 cursor-pointer" src={logo} alt="logo "/>
       <nav className="hidden md:block">
         <ul className="flex">
-          <li className=" menuItem mr-7 md:mr-8 hover:underline decoration-gray-700 ">Plant</li>
-          <li className="menuItem mr-7 md:mr-8 hover:underline decoration-gray-800 ">For offices</li>
-          <li className="menuItem mr-7 md:mr-8 hover:underline decoration-gray-900 ">Plants care</li>
-          <li className="menuItem hover:underline decoration-gray-500 lg:text-sm">About</li>
+          <li className=" menuItem mr-7 md:mr-8 hover:underline  decoration-gray-700 cursor-pointer">Plant</li>
+          <li className="menuItem mr-7 md:mr-8 hover:underline decoration-gray-700 cursor-pointer">For offices</li>
+          <li className="menuItem mr-7 md:mr-8 hover:underline decoration-gray-700 cursor-pointer">Plants care</li>
+          <li className="menuItem hover:underline  decoration-gray-700 cursor-pointer">About</li>
           
         </ul> 
       </nav>
       <div className="flex mb-1 ">
-        <img className="w-5 mr-8"src={searchIcon} alt="search"/>
-        <img className="w-5 mr-8" src={loginIcon} alt="login"/>
-        <img className="w-5" src={cartIcon} alt="cart"/>
+        <img className="w-5 mr-8 hover:animate-bounce cursor-pointer"src={searchIcon} alt="search"/>
+        <img className="w-5 mr-8 hover:animate-bounce cursor-pointer" src={loginIcon} alt="login"/>
+        <img className="w-5 hover:animate-bounce cursor-pointer" src={cartIcon} alt="cart"/>
       </div>
       <div className="block md:hidden">
         <img src={menuIcon} alt="menu"/>
